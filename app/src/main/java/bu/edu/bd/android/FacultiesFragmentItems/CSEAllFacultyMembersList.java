974d55ea6115e832c7e7b2a0fde43878a1b35de3
shakawat <shakawat_hossain@outlook.com>
2022-11-13 01:56:45 +0600

    no inforamtion from developer

--- app/src/main/java/bu/edu/bd/android/FacultiesFragmentItems/CSEAllFacultyMembersList.java
+++ app/src/main/java/bu/edu/bd/android/FacultiesFragmentItems/CSEAllFacultyMembersList.java
@@ -2,6 +2,8 @@ package bu.edu.bd.android.FacultiesFragmentItems;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.net.Uri;
@@ -9,13 +11,20 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.WindowManager;
 
+import java.util.ArrayList;
 import java.util.Objects;
 
 import bu.edu.bd.android.DashBoard.DashBoard;
+import bu.edu.bd.android.HomeFragmentItems.administration.BoardOfTrustAdapter;
+import bu.edu.bd.android.HomeFragmentItems.administration.BoardOfTrustModel;
+import bu.edu.bd.android.R;
 import bu.edu.bd.android.databinding.ActivityCseallFacultyMembersListBinding;
 
 public class CSEAllFacultyMembersList extends AppCompatActivity {
     ActivityCseallFacultyMembersListBinding binding;
+    RecyclerView recyclerView;
+    BoardOfTrustAdapter adapter;
+    ArrayList<BoardOfTrustModel> arrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +36,42 @@ public class CSEAllFacultyMembersList extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
+        arrayList= new ArrayList<>();
+        arrayList.add(new BoardOfTrustModel(R.drawable.default_profile_pic,getResources().getString(R.string.fSadiq)));
+        arrayList.add(new BoardOfTrustModel(R.drawable.default_profile_pic,getResources().getString(R.string.fAsraf)));
+        arrayList.add(new BoardOfTrustModel(R.drawable.default_profile_pic,getResources().getString(R.string.fYasir)));
+        arrayList.add(new BoardOfTrustModel(R.drawable.default_profile_pic,getResources().getString(R.string.fSahriar)));
+        arrayList.add(new BoardOfTrustModel(R.drawable.default_profile_pic,getResources().getString(R.string.fIbrahim)));
+        arrayList.add(new BoardOfTrustModel(R.drawable.default_profile_pic,getResources().getString(R.string.fRokeya)));
+        arrayList.add(new BoardOfTrustModel(R.drawable.default_profile_pic,getResources().getString(R.string.fSalma)));
+        arrayList.add(new BoardOfTrustModel(R.drawable.default_profile_pic,getResources().getString(R.string.fArifin)));
+        arrayList.add(new BoardOfTrustModel(R.drawable.default_profile_pic,getResources().getString(R.string.fAsraful)));
+        arrayList.add(new BoardOfTrustModel(R.drawable.default_profile_pic,getResources().getString(R.string.fSadia)));
+        arrayList.add(new BoardOfTrustModel(R.drawable.default_profile_pic,getResources().getString(R.string.fjakir)));
+        arrayList.add(new BoardOfTrustModel(R.drawable.default_profile_pic,getResources().getString(R.string.fTarique)));
+        arrayList.add(new BoardOfTrustModel(R.drawable.default_profile_pic,getResources().getString(R.string.fSaydur)));
+        arrayList.add(new BoardOfTrustModel(R.drawable.default_profile_pic,getResources().getString(R.string.fRiaz)));
+        arrayList.add(new BoardOfTrustModel(R.drawable.default_profile_pic,getResources().getString(R.string.fMahfuz)));
+        arrayList.add(new BoardOfTrustModel(R.drawable.default_profile_pic,getResources().getString(R.string.fJubayer)));
+        arrayList.add(new BoardOfTrustModel(R.drawable.default_profile_pic,getResources().getString(R.string.fSisir)));
+        arrayList.add(new BoardOfTrustModel(R.drawable.default_profile_pic,getResources().getString(R.string.fTariqul)));
+        arrayList.add(new BoardOfTrustModel(R.drawable.default_profile_pic,getResources().getString(R.string.fHasib)));
+        arrayList.add(new BoardOfTrustModel(R.drawable.default_profile_pic,getResources().getString(R.string.fIftekharul)));
+        arrayList.add(new BoardOfTrustModel(R.drawable.default_profile_pic,getResources().getString(R.string.fAfsana)));
+        arrayList.add(new BoardOfTrustModel(R.drawable.default_profile_pic,getResources().getString(R.string.fNazim)));
+        arrayList.add(new BoardOfTrustModel(R.drawable.default_profile_pic,getResources().getString(R.string.fAhammad)));
+        arrayList.add(new BoardOfTrustModel(R.drawable.default_profile_pic,getResources().getString(R.string.fShakil)));
+        arrayList.add(new BoardOfTrustModel(R.drawable.default_profile_pic,getResources().getString(R.string.fFazle)));
+        arrayList.add(new BoardOfTrustModel(R.drawable.default_profile_pic,getResources().getString(R.string.fAhsan)));
+        arrayList.add(new BoardOfTrustModel(R.drawable.default_profile_pic,getResources().getString(R.string.fAnik)));
+        arrayList.add(new BoardOfTrustModel(R.drawable.default_profile_pic,getResources().getString(R.string.fAsib)));
 
+
+        recyclerView= binding.facultyMembersRecycleView;
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+        adapter= new BoardOfTrustAdapter(getApplicationContext(),arrayList);
+        recyclerView.setAdapter(adapter);
     }
 
     @Override
